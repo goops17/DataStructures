4ee6115164eb90a5f3ed27c94e1fc7124bfcfbf3
Gopal from Nepal <35678561+goops17@users.noreply.github.com>
2018-02-05 10:27:47 -0600

    Create set.cpp

---
+++ STL/set.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <set>
+#include <sstream>
+using namespace std;
+set <string> dict;
+int main()
+{
+    string s,buf;
+    int i;
+    while(cin>>s){
+        for(i=0;i<s.length();i++){
+            if(isalpha(s[i])){
+                s[i]=tolower(s[i]);
+            }
+            else{
+                s[i]=' ';
+            }
+            stringstream ss(s);
+            while(ss>>buf){
+                dict.insert(buf);
+            }
+         }
+    }
+    for(set<string>::iterator it=dict.begin();it!=dict.end();++it){
+        cout<<*it<<"\n";
+    }
+    return 0;
+}
